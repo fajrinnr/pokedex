dd3373904dd3f883c0585dbe12daa080f2f184f9
fajrinnr <fajrin.noorrachman11@gmail.com>
2021-04-07 22:23:55 +0700

    Bug Fix - Fixing PWA

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,23 @@
 import "../styles/globals.css";
 import withData from "../config/apollo";
 import MyPokemonContext from "../src/contexts/myPokemonContext";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      window.addEventListener("load", () => {
+        navigator.serviceWorker
+          .register("/sw.js", { scope: "/" })
+          .then((registration) => {
+            console.log("SW registered: ", registration);
+          })
+          .catch((registrationError) => {
+            console.log("SW registration failed: ", registrationError);
+          });
+      });
+    }
+  }, []);
   return (
     <MyPokemonContext>
       <Component {...pageProps} />
